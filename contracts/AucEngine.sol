d3cf6534f78f0aedf3d4d6315844117cc0efcd70
evgeniykapelko <softwarearchitect19@gmail.com>
2023-05-21 14:21:07 +0300

    Auction

--- contracts/AucEngine.sol
+++ contracts/AucEngine.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /** 
  * @title AucEngine
- * @dev Implements voting process along with vote delegation
  */
 contract AucEngine {
     address public owner;
@@ -25,7 +24,8 @@ contract AucEngine {
     Auction[] public auctions;
 
     event AuctionCreated(uint index, string itemName, uint startingPrice, uint duration);
-    
+    event AuctionEnded(uint index, uint finalPrice, address winner);
+
     constructor() {
         owner = msg.sender;
     }
@@ -49,5 +49,42 @@ contract AucEngine {
         auctions.push(newAction);
 
         emit AuctionCreated(auctions.length - 1, _item, _startingPrice, duration);
+    }
+
+    function getPriceFor(uint index) public view returns(uint) {
+        Auction memory cAuction = auctions[index];
+
+        require(!cAuction.stopped, "stopped");
+
+        uint elapsed = block.timestamp - cAuction.startAt;
+        uint discount = cAuction.discountRate * elapsed;
+
+        return cAuction.startingPrice - discount;
+    }
+
+    function buy(uint index) external  payable {
+        Auction storage cAuction = auctions[index];
+
+        require(!cAuction.stopped, "stopped!");
+
+        require(block.timestamp < cAuction.endsAt, "ended!");
+
+        uint cPrice = getPriceFor((index));
+
+        require(msg.value >= cPrice, "not enough funds!");
+
+        cAuction.stopped = true;
+        cAuction.finalPrice = cPrice;
+        uint refund = msg.value - cPrice;
+
+        if (refund > 0) {
+            payable(msg.sender).transfer(refund);
+        }
+
+        cAuction.seller.transfer(
+            cPrice - ((cPrice * FEE) / 100)
+        );
+
+        emit AuctionEnded(index, cPrice, msg.sender);
     }
 }
